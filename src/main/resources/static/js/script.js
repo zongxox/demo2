7cb3d42706b33c697edf137d4f9fcb8f37cf92b0
zongxox <s520yi2930@gmail.com>
2025-04-22 23:56:44 +0800

    初始化

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,181 @@
+(function($) {
+
+  "use strict";
+
+  var initPreloader = function() {
+    $(document).ready(function($) {
+    var Body = $('body');
+        Body.addClass('preloader-site');
+    });
+    $(window).load(function() {
+        $('.preloader-wrapper').fadeOut();
+        $('body').removeClass('preloader-site');
+    });
+  }
+
+  // init Chocolat light box
+	var initChocolat = function() {
+		Chocolat(document.querySelectorAll('.image-link'), {
+		  imageSize: 'contain',
+		  loop: true,
+		})
+	}
+
+  var initSwiper = function() {
+
+    var swiper = new Swiper(".main-swiper", {
+      speed: 500,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    var category_swiper = new Swiper(".category-carousel", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".category-carousel-next",
+        prevEl: ".category-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        800: {
+          slidesPerView: 2,
+        },
+        1500: {
+          slidesPerView: 3,
+        },
+      }
+    });
+
+    $(".products-carousel").each(function(){
+      var $el_id = $(this).attr('id');
+
+      var products_swiper = new Swiper("#"+$el_id+" .swiper", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        speed: 500,
+        navigation: {
+          nextEl: "#"+$el_id+" .products-carousel-next",
+          prevEl: "#"+$el_id+" .products-carousel-prev",
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          991: {
+            slidesPerView: 4,
+          },
+        }
+      });
+
+    });
+
+    var swiper = new Swiper(".testimonial-Swiper", {
+      spaceBetween: 30,
+      pagination: {
+        el: ".testimonial-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        572: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+        1280: {
+          slidesPerView: 3,
+        },
+      },
+    });
+
+
+    // product single page
+    var thumb_slider = new Swiper(".product-thumbnail-slider", {
+      slidesPerView: 5,
+      spaceBetween: 20,
+      // autoplay: true,
+      direction: "vertical",
+      breakpoints: {
+        0: {
+          direction: "horizontal"
+        },
+        992: {
+          direction: "vertical"
+        },
+      },
+    });
+
+    var large_slider = new Swiper(".product-large-slider", {
+      slidesPerView: 1,
+      // autoplay: true,
+      spaceBetween: 0,
+      effect: 'fade',
+      thumbs: {
+        swiper: thumb_slider,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+
+  // input spinner
+  var initProductQty = function(){
+
+    $('.product-qty').each(function(){
+      
+      var $el_product = $(this);
+      var quantity = 0;
+      
+      $el_product.find('.quantity-right-plus').click(function(e){
+        e.preventDefault();
+        quantity = parseInt($el_product.find('#quantity').val());
+        $el_product.find('#quantity').val(quantity + 1);
+      });
+
+      $el_product.find('.quantity-left-minus').click(function(e){
+        e.preventDefault();
+        quantity = parseInt($el_product.find('#quantity').val());
+        if(quantity>0){
+          $el_product.find('#quantity').val(quantity - 1);
+        }
+      });
+
+    });
+
+  }
+
+  // init jarallax parallax
+  var initJarallax = function() {
+    jarallax(document.querySelectorAll(".jarallax"));
+
+    jarallax(document.querySelectorAll(".jarallax-keep-img"), {
+      keepImg: true,
+    });
+  }
+
+  // document ready
+  $(document).ready(function() {
+    
+    initPreloader();
+    initSwiper();
+    initProductQty();
+    initJarallax();
+    initChocolat();
+
+  }); // End of a document
+
+})(jQuery);
