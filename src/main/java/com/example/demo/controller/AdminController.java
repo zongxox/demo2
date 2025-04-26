205cfb8d10b7fd3f3d500f46551acc11341f3d0e
zongxox <s520yi2930@gmail.com>
2025-04-26 11:44:45 +0800

    更新

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -13,27 +13,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/admin")
 public class AdminController {
     @Autowired
     private AdminMapper adminMapper;
-
-
+    @Autowired
+    private UserMapper userMapper;
 
     //http://localhost:8080/login.html
     //登入
-    @PostMapping("/getAdminByAccountPassword")
-    public JsonResult getAdminByAccountPassword(@RequestBody Admin admin){
-        //接收前端傳過來的帳號密碼,查詢是否有該帳號
-        Admin adminByAccountPassword = adminMapper.getAdminByAccountPassword(admin.getAccount(), admin.getPassword());
-        System.out.println(adminByAccountPassword.getIs_admin());
-        if(adminByAccountPassword!=null){//判斷如果數據庫有該帳號及密碼
-            return JsonResult.ok(adminByAccountPassword);//成功
-        }else {//沒有則失敗
-            return new JsonResult(StatusCode.ACCOUNT_PASSWORD_ERROR);//帳號密碼錯誤
-        }
-    }
+    @PostMapping("/login")
+    public JsonResult login(@RequestBody Map<String, String> loginData) {
+        String account = loginData.get("account");
+        String password = loginData.get("password");
 
+        // 先查詢管理員
+        Admin admin = adminMapper.getAdminByAccountPassword(account, password);
+        if (admin != null) {//帳號,密碼,是否是管理員(1) true 代表不等於空值
+            // 管理員登入成功
+            admin.setPassword(null);//不回傳查到的密碼,用空值回傳給前端
+            return JsonResult.ok(admin); // 返回成功訊息
+        }
+
+        // 查詢普通用戶
+        User user = userMapper.getUserByAccountPassword(account, password);
+        if (user != null) {
+            // 普通用戶登入成功
+            user.setPassword(null);//不回傳查到的密碼,用空值回傳給前端
+            return JsonResult.ok(user); // 返回成功訊息
+        }
+
+        // 查無此用戶，返回錯誤訊息
+        return new JsonResult(StatusCode.ACCOUNT_PASSWORD_ERROR);//帳號密碼錯誤
+    }
 }
