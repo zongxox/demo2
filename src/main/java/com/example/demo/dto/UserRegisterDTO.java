b0d21113e4fc0cc3cbd795591b0da1955609127d
zongxox <s520yi2930@gmail.com>
2025-04-30 22:24:52 +0800

    更新

--- src/main/java/com/example/demo/dto/UserRegisterDTO.java
+++ src/main/java/com/example/demo/dto/UserRegisterDTO.java
@@ -12,5 +12,6 @@ public class UserRegisterDTO {
     private String name;
     private String phone;
     private String email;
-    private String reset_token;
+    private String reset_token; // 忘記密碼驗證 token
+    private String email_verify_token; //註冊驗證用 token
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -15,7 +15,12 @@ public class User {
     private String name;
     private String phone;
     private String email;
-    private Boolean is_admin;
+    private Boolean is_admin; //一般會員=0 管理員=1
+    private String reset_token; // 忘記密碼驗證 token
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date reset_token_expire; // 忘記密碼驗證 token 的過期時間
+    private String email_verify_token; //註冊驗證用 token
+    private Boolean email_verified; //是否已驗證 0=未驗證, 1=已驗證
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")  // 使用 @JsonFormat 來格式化返回的日期
     private Date created_time;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")  // 使用 @JsonFormat 來格式化返回的日期
